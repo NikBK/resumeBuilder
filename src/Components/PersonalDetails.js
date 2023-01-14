37acf415a34df17556850da561c608d1bf632a34
NikBK <nikhilbalakrishna5@gmail.com>
2023-01-14 18:08:31 +0530

    new changes with issues fixed

---
+++ src/Components/PersonalDetails.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import qa from './QuestionAnswers';
+
+const InputField = ({ placeholder, handleChange }) => {
+    const [showLegend, setShowLegend] = useState(false);
+    return <div className='input-element'>
+        <fieldset>
+            {showLegend && {} && <legend>{placeholder}</legend>}
+            <input
+                onChange={handleChange}
+                type='text'
+                id={placeholder}
+                placeholder={placeholder}
+                onClick={() => setShowLegend(true)} />
+        </fieldset>
+    </div>
+}
+
+const PersonalDetails = ({ setPage }) => {
+    const handleChange = (e) => {
+        qa.map(el => {
+            if (e.target.id === el.identity) {
+                return el.a = e.target.value;
+            }
+        })
+    }
+    return <>
+        <div id='input-fields-container'>
+            <InputField placeholder='Name*' handleChange={handleChange} />
+            <InputField placeholder='Email*' handleChange={handleChange} />
+            <InputField placeholder='Mobile*' handleChange={handleChange} />
+            <InputField placeholder='LinkedIn*' handleChange={handleChange} />
+            <InputField placeholder='Github*' handleChange={handleChange} />
+            <InputField placeholder='ex: Java 4, JS 3*' handleChange={handleChange} />
+        </div>
+        <Link to="/education">
+            <button id='next' onClick={() => setPage(prev => prev+1)}>
+                Next section
+            </button>
+        </Link>
+    </>
+}
+
+export default PersonalDetails;
---
+++ src/Components/QuestionAnswers.js
@@ -0,0 +1,124 @@
+const qa = [
+    // Personal details
+    {
+        q: 'Name',
+        a: '',
+        identity: 'Name*'
+    },
+    {
+        q: 'Email',
+        a: '',
+        identity: 'Email*'
+    },
+    {
+        q: 'Mobile',
+        a: '',
+        identity: 'Mobile*'
+    },
+    {
+        q: 'LinkedIn',
+        a: '',
+        identity: 'LinkedIn*'
+    },
+    {
+        q: 'Github',
+        a: '',
+        identity: 'Github*'
+    },
+    {
+        q: 'ex: Java 4, JS 3',
+        a: '',
+        identity: 'ex: Java 4, JS 3*'
+    },
+
+    // Education
+    {
+        q: 'School/College/University',
+        a: '',
+        identity: 'School/College/University*'
+    },
+    {
+        q: 'Degree',
+        a: '',
+        identity: 'Degree*'
+    },
+    {
+        q: 'Discipline',
+        a: '',
+        identity: 'Discipline*'
+    },
+    {
+        q: 'Marks/Percentage/CGPA',
+        a: '',
+        identity: 'Marks/Percentage/CGPA*'
+    },
+    {
+        q: 'Education From',
+        a: '',
+        identity: 'Education From*'
+    },
+    {
+        q: 'Education To',
+        a: '',
+        identity: 'Education To*'
+    },
+
+    // Experience
+    {
+        q: 'Organization',
+        a: '',
+        identity: 'Organization*'
+    },
+    {
+        q: 'Position',
+        a: '',
+        identity: 'Position*'
+    },
+    {
+        q: 'Present Job',
+        a: '',
+        identity: 'Present Job'
+    },
+    {
+        q: 'Experience From',
+        a: '',
+        identity: 'Experience From*'
+    },
+    {
+        q: 'Experience To',
+        a: '',
+        identity: 'Experience To*'
+    },
+    {
+        q: 'Experience Description',
+        a: '',
+        identity: 'Experience Description*'
+    },
+
+    // Project
+    {
+        q: 'Title',
+        a: '',
+        identity: 'Title*'
+    },
+    {
+        q: 'Link',
+        a: '',
+        identity: 'Link*'
+    },
+    {
+        q: 'Project Description',
+        a: '',
+        identity: 'Project Description*'
+    },
+
+    // Hobbies
+    {
+        q: 'Hobbies Description',
+        a: '',
+        identity: 'Hobbies Description*'
+    }
+
+]
+
+export default qa;
